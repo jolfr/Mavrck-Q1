c7b7097bdaadb1162f039cfcc34d7b2271b3e24d
jolfr <thomas.jack.carroll@gmail.com>
2021-04-08 10:13:56 -0400

    added default rest response

--- src/main/kotlin/mavrck/question/one/OneApplication.kt
+++ src/main/kotlin/mavrck/question/one/OneApplication.kt
@@ -2,6 +2,8 @@ package mavrck.question.one
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class OneApplication
@@ -9,3 +11,15 @@ class OneApplication
 fun main(args: Array<String>) {
 	runApplication<OneApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
+
+data class Message(val id: String?, val text: String)
