ccc7e974c25087f9e3062a7d936f0cbeeef851d1
jolfr <thomas.jack.carroll@gmail.com>
2021-04-08 09:56:31 -0400

    initialized from spring initializer

---
+++ src/test/kotlin/mavrck/question/one/OneApplicationTests.kt
@@ -0,0 +1,13 @@
+package mavrck.question.one
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
