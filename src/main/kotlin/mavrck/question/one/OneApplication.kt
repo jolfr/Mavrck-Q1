c1bb13a3b620081fe65fdc8691dbd8d70f8f03e9
jolfr <thomas.jack.carroll@gmail.com>
2021-04-09 16:23:40 -0400

    declared groupby resource

--- src/main/kotlin/mavrck/question/one/OneApplication.kt
+++ src/main/kotlin/mavrck/question/one/OneApplication.kt
@@ -5,11 +5,23 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+
+val PATHNAME= "/home/jolfr/Projects/Mavrck-Q1/src/main/resources/CrunchBase-Companies-2017-09-05.csv"
 @SpringBootApplication
 class OneApplication
 
 fun main(args: Array<String>) {
 	runApplication<OneApplication>(*args)
+}
+/**
+ * Endpoint to run groupby and count
+ */
+class GroupByCountResource {
+    val aggregate: AggregationFunctions = AggregationFunctions(1) // Initialize aggregation object with number of columns to aggregate over
+    val groupByCount = aggregate.GroupByCountFactory() // Call factory method to get GroupBy and Count
+    val processor: FileProcessor = FileProcessor(PATHNAME, groupByCount) // Initialize file processor object with specific aggregation function
+
+    processor.iterateRows() // Where the magic happens
 }
 
 @RestController
