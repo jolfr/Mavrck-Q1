df3313785931c33b07a4c98fef974c3b71a2bc31
jolfr <thomas.jack.carroll@gmail.com>
2021-04-09 10:12:43 -0400

    defined iterate rows function

--- src/main/kotlin/mavrck/question/one/FileProcessor.kt
+++ src/main/kotlin/mavrck/question/one/FileProcessor.kt
@@ -1,11 +1,30 @@
 package mavrck.question.one
 
 import java.io.* // Fix this lazy import
+import java.io.FileNotFoundException
+import java.lang.IllegalArgumentException
 /**
  * This class parses the CSV row by row to allow aggregations of the data.
  * @param filepath absolute path of CSV file to process
  * @param aggFunc aggregation function to act on CSV file
  */
-class FileProcessor(val filepath: String, val aggFunc: () -> Unit) {
+class FileProcessor(val filepath: String, val aggFunc: (cells: Array<List<String>?>) -> Unit) {
 
+    /**
+     * Iterate through rows, applying aggregation function for each row
+     * NOTE: could pass in agg function here to allow multiple aggregations
+     */
+    fun iterateRows() {
+        var csvReader: BufferedReader? // initialize empty var outside try block for proper scope
+        try {
+            csvReader = BufferedReader(FileReader(filepath));
+        } catch (exception: FileNotFoundException) {
+            throw IllegalArgumentException(exception.message);
+        } 
+        var line: String? = null
+        while ({ line = csvReader.readLine(); line }() != null) { // I have a vague understanding of why {} works in this instance, some clarification would be appreciated
+            var cells = arrayOf(line?.split(",")); // Parses comma seperated values in row
+            aggFunc(cells); // 
+        }
+    }
 }
