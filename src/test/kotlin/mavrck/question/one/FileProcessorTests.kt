c9cc1f31a5eaf565cf7e9dea5bf133ddc4953f39
jolfr <thomas.jack.carroll@gmail.com>
2021-04-09 11:13:21 -0400

    added bad path exception test

--- src/test/kotlin/mavrck/question/one/FileProcessorTests.kt
+++ src/test/kotlin/mavrck/question/one/FileProcessorTests.kt
@@ -1,13 +1,27 @@
 package mavrck.question.one
 
+import java.lang.IllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+
 
 class FileProcessorTests {
     @Test
     fun `object initializes`() {
-        val testAggFunc = fun (_ : Array<List<String>?>){}
+        val testAggFunc = fun (_ : Array<List<String>?>){} // MOCKS!!!
         val processor = FileProcessor("testPath", testAggFunc)
         assertNotNull(processor)
+    }
+
+    fun `iterate rows with proper path`() {
+
+    }
+
+    fun `iterate rows throws IllegalArgumentException on bad path`() {
+        val testAggFunc = fun (_ : Array<List<String>?>){} // MOCKS!!!
+        assertThrows(IllegalArgumentException::class.java) {
+            val processor = FileProcessor("BADPATH", testAggFunc)
+            processor.iterateRows()
+        }
     }
 }
