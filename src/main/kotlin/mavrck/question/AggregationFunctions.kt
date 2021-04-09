378c61ee0b12226c3b97e86709d91fd58ff79de5
jolfr <thomas.jack.carroll@gmail.com>
2021-04-09 13:42:37 -0400

    added stub definition for group by count factory

--- src/main/kotlin/mavrck/question/AggregationFunctions.kt
+++ src/main/kotlin/mavrck/question/AggregationFunctions.kt
@@ -5,5 +5,17 @@ package mavrck.question.one
  * @param columns number of columns to perform aggregation on
  */
 class AggregationFunctions(val columns: Int) {
-    
+
+    /**
+     * Constructs function to run groupby and count on row iterations
+     * @return function that performs gorupby and count, returning result
+     */
+    fun GroupByCountFactory():(ArrayList<String>) -> Unit {
+        if (columns < 1) throw IllegalArgumentException("Column count must be greater than 0") // This is sloppy, need to check arguments at object init
+
+        val groupByCount = fun():Int{
+            return 0
+        }
+        return groupByCount.invoke()
+    }
 }
