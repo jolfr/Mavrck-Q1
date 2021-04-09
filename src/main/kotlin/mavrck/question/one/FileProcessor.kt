6fa29c206bb7e9948033714a0f9db9abfebe790b
jolfr <thomas.jack.carroll@gmail.com>
2021-04-09 16:19:19 -0400

    fixed aggregation function call

--- src/main/kotlin/mavrck/question/one/FileProcessor.kt
+++ src/main/kotlin/mavrck/question/one/FileProcessor.kt
@@ -3,28 +3,27 @@ package mavrck.question.one
 import java.io.* // Fix this lazy import
 import java.io.FileNotFoundException
 import java.lang.IllegalArgumentException
+import kotlin.reflect.*
 /**
  * This class parses the CSV row by row to allow aggregations of the data.
  * @param filepath absolute path of CSV file to process
- * @param aggFunc aggregation function to act on CSV file
  */
-class FileProcessor(val filepath: String, val aggFunc: (cells: Array<List<String>?>) -> Unit) {
-
+class FileProcessor(val filepath: String) {
     /**
      * Iterate through rows, applying aggregation function for each row
-     * NOTE: could pass in agg function here to allow multiple aggregations
+     * @param aggFunc aggregation function to act on CSV file
      */
-    fun iterateRows() {
+    fun iterateRows(aggFunc: KFunction<Any>) {
         var csvReader: BufferedReader? // initialize empty var outside try block for proper scope
-        try {
+        try { // NOTE this should execute in parent class constructor instead
             csvReader = BufferedReader(FileReader(filepath));
         } catch (exception: FileNotFoundException) {
-            throw IllegalArgumentException(exception.message);
+            throw IllegalArgumentException(exception.message); // SLOPPY! Illegal argument in parent instantiation
         } 
         var line: String? = null
         while ({ line = csvReader.readLine(); line }() != null) { // I have a vague understanding of why {} works in this instance, some clarification would be appreciated
             var cells = arrayOf(line?.split(",")); // Parses comma seperated values in row
-            aggFunc(cells);
+            aggFunc.call(cells)
         }
     }
 }
