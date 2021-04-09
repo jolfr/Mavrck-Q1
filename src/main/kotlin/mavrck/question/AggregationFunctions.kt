59b61e5f1d4abac1113c324157359cbfa2750664
jolfr <thomas.jack.carroll@gmail.com>
2021-04-09 13:08:30 -0400

    added stub for aggregation functions

---
+++ src/main/kotlin/mavrck/question/AggregationFunctions.kt
@@ -0,0 +1,9 @@
+package mavrck.question.one
+
+/**
+ * Contains factories to construct various aggregation functions
+ * @param columns number of columns to perform aggregation on
+ */
+class AggregationFunctions(val columns: Int) {
+    
+}
