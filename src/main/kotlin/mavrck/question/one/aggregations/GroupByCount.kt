070aa1b2e0180d0e6fe2cc46e67a4a7a3383296d
jolfr <thomas.jack.carroll@gmail.com>
2021-04-12 11:06:31 -0400

    filled out docstrings

--- src/main/kotlin/mavrck/question/one/aggregations/GroupByCount.kt
+++ src/main/kotlin/mavrck/question/one/aggregations/GroupByCount.kt
@@ -12,6 +12,7 @@ open class GroupByCountFactory(): AggFuncFactory() { // classes have to be open 
 /**
  * Concrete implmenetation of interface AggFunc
  * @param colsOfInterest set of column numbers to perform aggregations on
+ * @return function which is ready to be used by a file processor
  */
 open class GroupByCount(colsOfInterest: Set<Int>): AggFunc {
     override val columns: Set<Int>
@@ -20,17 +21,32 @@ open class GroupByCount(colsOfInterest: Set<Int>): AggFunc {
     }
     var aggEntries: MutableMap<List<String>, Int> = mutableMapOf()
 
+    /**
+     * Processes a single row of the dataframe by first getting the cells of interest,
+     * and then checking against the existing list with AddOrAppend
+     * @param row 
+     */
     override fun processRow(row: List<String>?): Unit {
         val entries = getEntriesOfInterest(row)
         AddOrAppend(entries!!)
     }
 
+    /**
+     * Helper function. Filters row object by columns selected by user
+     * @param row
+     * @return Same object, just with fewer entries
+     */
     fun getEntriesOfInterest(row: List<String>?): List<String>? {
         var result = mutableListOf<String>()
         columns.forEach{ result.add(row!!.get(it)) }
         return result
     }
 
+    /**
+     * Helper function. If entries exists in the final structure, then increment that count by one. 
+     * Otherwise, append structure to end of the list.
+     * @param entries Entries of columns that have been previously selected
+     */
     fun AddOrAppend(entries: List<String>) {
         var count: Int
         if(aggEntries.containsKey(entries)) {
