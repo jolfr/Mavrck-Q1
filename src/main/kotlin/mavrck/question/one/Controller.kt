b57132e9fde135d2d414928937527224fd3e1838
jolfr <thomas.jack.carroll@gmail.com>
2021-04-12 04:18:26 -0400

    added functions and tests for controller

--- src/main/kotlin/mavrck/question/one/Controller.kt
+++ src/main/kotlin/mavrck/question/one/Controller.kt
@@ -1,5 +1,37 @@
 package mavrck.question.one
 
-class GroupByCountController() {
-    
+/**
+ * Initiates the two objects required to run the GroupBy action and runs, returning the result
+ * @param filepath the absolute path to the .csv file to run the aggregation on
+ * @param colsOfInterest unordered list of column numbers WARNING not robust to edge cases
+ */
+class GroupByCountController(filepath: String, colsOfInterest: Set<Int>, ) {
+    val processor: FileProcessor
+    val factory: GroupByCountFactory
+    val function: GroupByCount
+    init {
+        try {
+            processor = FileProcessor(filepath)
+            factory = GroupByCountFactory()
+            function = factory.getFunction(colsOfInterest)
+            
+        } catch (e: Exception) {
+            throw RuntimeException("controller initialization failed with message: " + e.message)
+        }
+    }
+
+    /**
+     * Wrapper to call iterate rows after initialization
+     */
+    fun executeAggregation() {
+        processor.iterateRows(function)
+    }
+
+    /**
+     * Call into function object to retrieve result. 
+     * @return mapped object with column names as the key and count as the value
+     */
+    fun getResult(): MutableMap<List<String>, Int> {
+        return function.aggEntries
+    }
 }
--- src/test/kotlin/mavrck/question/one/ControllerTests.kt
+++ src/test/kotlin/mavrck/question/one/ControllerTests.kt
@@ -1,5 +1,21 @@
 package mavrck.question.one
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions.*
+
 class GrooupByCountControllerTests() {
-    
+    val FILEPATH = "/home/jolfr/Projects/Mavrck-Q1/src/main/resources/100 Sales Records.csv"
+    val columns = listOf(1,2,3,4,5,6,7).toSet()
+    lateinit var testController: GroupByCountController
+    @BeforeEach
+    fun setup() {
+        testController = GroupByCountController(FILEPATH, columns)
+    }
+
+
+    fun `initialization succeeds`() {
+        assertNotNull(testController)
+    }
+
 }
