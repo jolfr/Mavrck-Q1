ccec00d17a08cce358f26be5a744c0d1878a973a
jolfr <thomas.jack.carroll@gmail.com>
2021-04-11 22:05:31 -0400

    added columns of interest to agg functions

--- src/main/kotlin/mavrck/question/one/AggregationFunction.kt
+++ src/main/kotlin/mavrck/question/one/AggregationFunction.kt
@@ -1,11 +1,14 @@
 package mavrck.question.one
 
 import kotlin.reflect.*
+import kotlin.collections.MutableMap
+import kotlin.collections.MutableMap.MutableEntry
 
 interface AggFunc {
+    val columns: Set<Int>
     fun processRow(row: Array<List<String>?>): Unit
 }
 
 abstract class AggFuncFactory {
-    abstract fun getFunction(): AggFunc
+    abstract fun getFunction(colsOfInterest: Set<Int>): AggFunc
 }
--- src/main/kotlin/mavrck/question/one/aggregations/GroupByCount.kt
+++ src/main/kotlin/mavrck/question/one/aggregations/GroupByCount.kt
@@ -1,10 +1,18 @@
 package mavrck.question.one
+
+import kotlin.collections.MutableMap
+import kotlin.collections.MutableMap.MutableEntry
 // classes have to be open for mockito :(
 open class GroupByCountFactory(): AggFuncFactory() {
-    override fun getFunction() = GroupByCount()
+    override fun getFunction(colsOfInterest: Set<Int>) = GroupByCount(colsOfInterest)
 }
 
-open class GroupByCount: AggFunc {
+open class GroupByCount(colsOfInterest: Set<Int>): AggFunc {
+    override val columns: Set<Int>
+    init {
+        columns = colsOfInterest
+    }
+
     override fun processRow(row: Array<List<String>?>): Unit {
         println(row.size)
     }
